57a9c6286cef1010cfcf40dba0a407a2bc32c455
George Chen (Air) <sylvanknave@gmail.com>
2015-07-10 08:31:45 -0500

    Implemented WoW API

--- get.js
+++ get.js
@@ -1,6 +1,44 @@
 var async = require('async');
+var fs = require('fs');
 
 var wow = require('./wow');
 
+var saveToFile = function (filePath, data) {
+  fs.writeFile(
+    filePath,
+    JSON.stringify(data, null, 2),
+    function(error) {
+      if (error) {
+        console.error(error);
+      }
+      else {
+        console.log('Saved ' + filePath);
+      }
+    }
+  );
+}
 
+
+wow.getLeaderboards(
+  '2v2',
+  function (data) {
+    saveToFile('2v2.json', data);
+  },
+  function (error) {
+    console.error(error);
+  }
+);
+
+
+wow.getCharacterItems(
+  'emerald-dream',
+  'ojbect',
+  function (data) {
+
+    saveToFile('object.json', data);
+  },
+  function (error) {
+    console.error(error);
+  }
+);
 
--- wow.js
+++ wow.js
@@ -1,50 +1,104 @@
+var http = require('http');
 var https = require('https');
+var querystring = require('querystring');
 var API_KEY = require('./secret').BNET_API_KEY;
 var DOMAIN = 'us.api.battle.net';
 
-var wowRequest = function (method, path) {
+var WowApi = {};
+
+WowApi.get = function (path, onSuccess, onError, arguments) {
+
+  arguments = arguments || {};
+  arguments.apikey = API_KEY;
+  arguments.locale = 'en_US';
+
+  var fullPath = '/wow' + path + '?' + querystring.stringify(arguments);
+
+  console.log(fullPath)
+
   var options = {
-    method: method,
+    method: 'GET',
     hostname: DOMAIN,
-    port: 80,
-    path: path + '?api_key=' + API_KEY
+    // port: 80,
+    path: fullPath
   };
 
   var request = https.request(
     options,
-    function(response) {
-      response.setEncoding('utf8');
-      response.on('data',
-        function (chunk) {
-          console.log(chunk);
-        }
-      )
+    function (data) {
+      processResponse(data, onSuccess, onError);
     }
   );
 
   request.end();
 }
 
+// helper
+function processResponse( response, onSuccess, onError ) {
+  var json = null;
+  var body = '';
+  var status = response.statusCode;
 
+  if ( 200 === status ) {
+    response.setEncoding('utf8');
+
+    // Combine data received
+    response.on('data', function(data) {
+      body += data;
+    });
+
+    response.on('end', function() {
+      try {
+        json = JSON.parse(body);
+      }
+      catch (exception) {
+        onError('Could not parse ' + body);
+      }
+
+      if (null !== json) {
+        onSuccess(json);
+      }
+    });
+
+  }
+  else {
+     onError(status + ': ' + http.STATUS_CODES[status]);
+  }
+}
+
+
+WowApi.BRACKETS = ['2v2', '3v3', '5v5', 'rbg'];
 /**
  * @param bracket 2v2, 3v3, 5v5, rbg
  */
-var getLeaderBoards = function (bracket) {
+WowApi.getLeaderboards = function (bracket, onSuccess, onError) {
   if ( 'string' === typeof bracket &&
-    -1 != ['2v2', '3v3', '5v5', 'rbg'].indexOf(bracket) ) {
+    -1 != WowApi.BRACKETS.indexOf(bracket) ) {
 
-
-
-
-
+    WowApi.get(
+      '/leaderboard/' + bracket,
+      onSuccess,
+      onError
+    );
   }
   else {
     throw 'Bracket is wrong';
   }
 }
 
-var getCharacterItems = function (name) {
+WowApi.getCharacter = function (realm, characterName, onSuccess, onError, field) {
 
+  WowApi.get(
+    '/character/' + realm + '/' + characterName,
+    onSuccess,
+    onError,
+    { 'fields': field }
+  );
+}
+
+WowApi.getCharacterItems = function (realm, characterName, onSuccess, onError) {
+  WowApi.getCharacter(realm, characterName, onSuccess, onError, 'items');
 }
 
 
+module.exports = WowApi;
